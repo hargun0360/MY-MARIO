f998ed93c54d4ae4a5d1819d359f510fc104adca
=Hargun Singh <=Hargun0360@gmail.com>
2021-10-15 02:20:25 +0530

    add mario up and down

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ function play() {
     var m=0;
     var x=document.getElementById('bird');
     x.style.bottom=((Math.random()*60 + 60)) + "px";
+    var flag=1;
     
     setInterval(function enemy(){
         var bird=['images/bird1.png','images/bird2.png'];
@@ -33,6 +34,8 @@ function play() {
 
     window.addEventListener('keydown', function (event) {
         if (event.key === "ArrowRight") {
+            mario.style.transform="scale(1.0)";
+            flag=1;
              var img = ['images/mario2.png', 'images/mario3.png', 'images/mario4.png'];
             if (mario.offsetLeft < (window.innerWidth - 100)) {
                 mario.style.left = mario.offsetLeft + pos + "px";
@@ -46,7 +49,48 @@ function play() {
     })
 
     window.addEventListener('keydown', function (event) {
+        if (event.key === "ArrowDown") {
+
+            
+             
+            if (flag==1) {
+                mario.style.backgroundImage = 'url(images/downr.png)';
+                mario.style.transform="scale(0.8)";
+                mario.style.bottom="50px";
+            }
+            else if(flag==0){
+                mario.style.backgroundImage = 'url(images/downl.png)';
+                mario.style.transform="scale(0.8)";
+                mario.style.bottom="50px";
+            }
+        }
+    })
+
+
+
+    window.addEventListener('keydown', function (event) {
+        if (event.key === "ArrowUp") {
+
+            
+             
+            if (flag==1) {
+                mario.style.backgroundImage = 'url(images/mario1.png)';
+                mario.style.transform="scale(1.0)";
+                mario.style.bottom="60px";
+            }
+            else if(flag==0){
+                mario.style.backgroundImage = 'url(images/mario5.png)';
+                mario.style.transform="scale(1.0)";
+                mario.style.bottom="60px";
+            }
+        }
+    })
+
+    window.addEventListener('keydown', function (event) {
+        
         if (event.key === "ArrowLeft") {
+            flag=0;
+            mario.style.transform="scale(1.0)";
             var image = ['images/mario5.png', 'images/mario6.png', 'images/mario7.png', 'images/mario8.png'];
             if (mario.offsetLeft > 0) {
                 mario.style.left = mario.offsetLeft - pos + "px";
@@ -60,18 +104,29 @@ function play() {
     })
 
     window.addEventListener('keydown',function(event){
+        
 
         if(event.key===" "){
+            mario.style.transform="scale(1.0)";
 
+            
+            if(flag==1){
+                mario.style.backgroundImage='url(images/mario1.png)';
 
-            if(mario.offsetTop==465){
-                mario.style.bottom=200 + "px";
+            }
+            if(flag==0){
+
+                mario.style.backgroundImage='url(images/mario5.png)';
+
+            }
+            if(mario.offsetTop>=465){
+                mario.style.bottom=250 + "px";
 
                 setTimeout(function jump(){
 
                     mario.style.bottom="60px";
 
-                },300);
+                },350);
                
 
             }
